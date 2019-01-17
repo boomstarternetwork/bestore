46a7e7bc236a57423972e0818714021ad4b923f6
Vadim Chernov <v.chernov@boomstarter.ru>
2019-01-17 20:58:21 +0300

    save point

--- mockstore.go
+++ mockstore.go
@@ -1,6 +1,7 @@
 package bestore
 
 import (
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -12,8 +13,9 @@ func NewMockStore() *MockStore {
 	return &MockStore{}
 }
 
-func (ms *MockStore) AddUser(externalID, email, password, name, avatarURL string) (User, error) {
-	args := ms.Called(externalID, email, password, name, avatarURL)
+func (ms *MockStore) AddUser(externalID, email, password, name,
+	avatarURL string, ethAddress string) (User, error) {
+	args := ms.Called(externalID, email, password, name, avatarURL, ethAddress)
 	return args.Get(0).(User), args.Error(1)
 }
 
@@ -71,6 +73,44 @@ func (ms *MockStore) GetUsers() ([]User, error) {
 	return users.([]User), args.Error(1)
 }
 
+func (ms *MockStore) CheckProjectCategoryID(categoryID uint) error {
+	args := ms.Called(categoryID)
+	return args.Error(0)
+}
+
+func (ms *MockStore) AddCountry(id uint, name string) error {
+	args := ms.Called(id, name)
+	return args.Error(0)
+}
+
+func (ms *MockStore) GetCountries() ([]Country, error) {
+	args := ms.Called()
+	cs := args.Get(0)
+	if cs == nil {
+		return nil, args.Error(1)
+	}
+	return cs.([]Country), args.Error(1)
+}
+
+func (ms *MockStore) AddCity(id uint, countryID uint, name string) error {
+	args := ms.Called(id, countryID, name)
+	return args.Error(0)
+}
+
+func (ms *MockStore) GetCities(countryID uint) ([]City, error) {
+	args := ms.Called(countryID)
+	cs := args.Get(0)
+	if cs == nil {
+		return nil, args.Error(1)
+	}
+	return cs.([]City), args.Error(1)
+}
+
+func (ms *MockStore) CheckCityID(cityID uint) error {
+	args := ms.Called(cityID)
+	return args.Error(0)
+}
+
 func (ms *MockStore) GetUserPasswordReset(code string) (UserPasswordReset, error) {
 	args := ms.Called(code)
 	return args.Get(0).(UserPasswordReset), args.Error(1)
@@ -102,31 +142,92 @@ func (ms *MockStore) RemoveUserEmailConfirmation(userID uint) error {
 	return args.Error(0)
 }
 
+func (ms *MockStore) GetUserKYC(userID uint) (UserKYC, error) {
+	args := ms.Called(userID)
+	return args.Get(0).(UserKYC), args.Error(1)
+}
+
+func (ms *MockStore) SetUserKYC(kyc UserKYC) error {
+	args := ms.Called(kyc)
+	return args.Error(0)
+}
+
+func (ms *MockStore) SetUserMiningCredential(userID uint,
+	login string) (string, error) {
+	args := ms.Called(userID, login)
+	return args.String(0), args.Error(1)
+}
+
+func (ms *MockStore) GetUserMiningProject(userID uint) (UserMiningProject,
+	error) {
+	args := ms.Called(userID)
+	return args.Get(0).(UserMiningProject), args.Error(1)
+}
+
+func (ms *MockStore) SetUserMiningProject(userID uint, projectID uint) error {
+	args := ms.Called(userID, projectID)
+	return args.Error(0)
+}
+
+func (ms *MockStore) GetUserWithdraw(userID uint) (UserWithdraw, error) {
+	args := ms.Called(userID)
+	return args.Get(0).(UserWithdraw), args.Error(1)
+}
+
+func (ms *MockStore) SetUserWithdraw(userID uint, status OperationStatus,
+	amount decimal.Decimal) error {
+	args := ms.Called(userID, status, amount)
+	return args.Error(0)
+}
+
+func (ms *MockStore) RemoveUserWithdraw(userID uint) error {
+	args := ms.Called(userID)
+	return args.Error(0)
+}
+
 func (ms *MockStore) GetProject(projectID uint) (Project, error) {
 	args := ms.Called(projectID)
 	return args.Get(0).(Project), args.Error(1)
 }
 
-func (ms *MockStore) AddProject(projectName string) error {
-	args := ms.Called(projectName)
+func (ms *MockStore) AddProject(p Project) (Project, error) {
+	args := ms.Called(p)
+	return args.Get(0).(Project), args.Error(1)
+}
+
+func (ms *MockStore) SetProject(p Project) (Project, error) {
+	args := ms.Called(p)
+	return args.Get(0).(Project), args.Error(1)
+}
+
+func (ms *MockStore) SetProjectModerationStatus(projectID uint,
+	moderationStatus OperationStatus) error {
+	args := ms.Called(projectID, moderationStatus)
 	return args.Error(0)
 }
 
-func (ms *MockStore) SetProjectName(projectID uint, name string) error {
-	args := ms.Called(projectID, name)
-	return args.Error(0)
+func (ms *MockStore) GetProjects(limit uint, offset uint, userID uint,
+	categoryID uint, statuses []ProjectStatus) ([]Project, uint, error) {
+	args := ms.Called()
+	projects := args.Get(0)
+	count := args.Get(1).(uint)
+	if projects == nil {
+		return nil, count, args.Error(2)
+	}
+	return projects.([]Project), count, args.Error(2)
 }
 
-func (ms *MockStore) RemoveProject(projectID uint) error {
-	args := ms.Called(projectID)
-	return args.Error(0)
-}
-
-func (ms *MockStore) GetProjects() ([]Project, error) {
+func (ms *MockStore) GetProjectCategories() ([]ProjectCategory, error) {
 	args := ms.Called()
 	projects := args.Get(0)
 	if projects == nil {
 		return nil, args.Error(1)
 	}
-	return projects.([]Project), args.Error(1)
+	return projects.([]ProjectCategory), args.Error(1)
+}
+
+func (ms *MockStore) GetMinedByUser(projectID uint,
+	userID uint) (decimal.Decimal, error) {
+	args := ms.Called(projectID, userID)
+	return args.Get(0).(decimal.Decimal), args.Error(1)
 }
